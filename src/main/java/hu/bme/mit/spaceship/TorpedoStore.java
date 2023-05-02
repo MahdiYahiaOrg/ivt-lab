072224265ac17c0e49e3f20576ea4912a08551b3
MahdiYahia <112518287+MahdiYahia@users.noreply.github.com>
2023-05-02 23:03:24 +0200

    Save and re-use random

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -27,7 +27,7 @@ public class TorpedoStore {
       }
     }
   }
-
+  Random generator;
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       println("No");
@@ -36,7 +36,7 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
+    generator = new Random();
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
